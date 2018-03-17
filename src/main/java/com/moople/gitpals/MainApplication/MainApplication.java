8f63b6edde886a918b76071d5832f0718612870d
dan322 <dandurnev0@gmail.com>
2018-03-17 13:14:13 +0300

    Update main executing file

--- src/main/java/com/moople/gitpals/MainApplication/MainApplication.java
+++ src/main/java/com/moople/gitpals/MainApplication/MainApplication.java
@@ -1,5 +1,6 @@
 package com.moople.gitpals.MainApplication;
 
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+		MongoClient mongoClient = new MongoClient("mongodb://danmoop_admin:hackingtheinternet@ds215759.mlab.com:15759/gitpals", 15759);
 	}
 
 }
