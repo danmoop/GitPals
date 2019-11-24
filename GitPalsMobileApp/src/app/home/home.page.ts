76145aa499c6c5a986a9af6fb2d8848fa8a36cd5
Daniil Durnev <dandurnev1@gmail.com>
2019-11-24 08:49:32 -0800

    authentication has been implemented

--- GitPalsMobileApp/src/app/home/home.page.ts
+++ GitPalsMobileApp/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import axios from 'axios';
 import { localService } from './../localService';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -12,17 +13,22 @@ export class HomePage {
   PROJECT_LIMIT = 25;
   projects = [];
 
-  constructor(private service: localService) {}
+  user: any;
+
+  constructor(private service: localService, private alertCtrl: AlertController) {}
 
   ionViewDidEnter() {
     this.fetchProjects(null);
+
+    if(localStorage.getItem('user') != null) {
+      this.user = JSON.parse(localStorage.getItem('user'));
+    }
   }
 
   fetchProjects(event) {
     axios.get(this.service.getAPI() + '/projects/getAmount/' + this.PROJECT_LIMIT)
       .then(response => {
         this.projects = response.data.reverse();
-        console.log(response.data[0]);
 
         if(event != null) {
           event.target.complete();
@@ -34,5 +40,52 @@ export class HomePage {
     return project.requirements.join(', ');
   }
 
+  getProjectInfo(project) {
+    console.log(project);
+  }
 
+  auth() {
+    this.alertCtrl.create({
+      header: 'Sign In to GitHub',
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Username',
+        },
+        {
+          name: 'password',
+          placeholder: 'Password',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'OK',
+          handler: (data) => {
+            axios('https://api.github.com/user', {
+              method: 'GET',
+              auth: {
+                username: data.username,
+                password: data.password
+              }
+            }).then(response => {
+              this.user = response.data;
+              localStorage.setItem('user', JSON.stringify(this.user));
+            }).catch(err => this.wrongCredentialsAlert());
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    }).then(alert => alert.present());
+  }
+
+  wrongCredentialsAlert() {
+    this.alertCtrl.create({
+      header: 'Wrong Credentials',
+      buttons: ['OK']
+    }).then(alert => alert.present());
+  }
 }
